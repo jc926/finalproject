8ed296dfeac5db61a86fbc1ac5227f39a4f420ae
jie cai <jc926@afsconnect1.njit.edu>
2017-12-17 16:06:41 -0500

    still test3

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -23,18 +23,18 @@ class tasksController extends http\controller
     public static function all()
     {
         $records = todos::findAll();
-        /*session_start();
-           if(key_exists('userID',$_SESSION)) {
-               $userID = $_SESSION['userID'];
-           } else {
-
-               echo 'you must be logged in to view tasks';
-           }
+        if(!isset($_SESSION['userID'])) {
+            echo " didn't login";
+            exit();
+        }
         $userID = $_SESSION['userID'];
 
         $records = todos::findTasksbyID($userID);
-        */
-        self::getTemplate('all_tasks', $records);
+        if($records) {
+            self::getTemplate('all_tasks', $records);
+        } else {
+            self::create();
+
 
     }
     //to call the show function the url is called with a post to: index.php?page=task&action=create
